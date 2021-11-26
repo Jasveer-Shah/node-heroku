14528477c49666d3567cd29fe1f7a4cac098f9f4
jasveer shah <talkjasveer@gmail.com>
2021-11-25 22:20:52 -0500

    routes, views with express

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+const http = require("http")
 const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require('./routes/admin');
+const shopRoute = require('./routes/shop')
 const app = express();
 
 
-app.get("/", function(req, res) {
-    res.send("got the response from express!");
+           // handling different routes
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use("/admin", adminRoutes);
+  app.use(shopRoute);
+//   app.use("/", (req, res, next) => {
+//       console.log("this always runs");
+//       next();
+//   })
+  
+  
+
+
+//   app.use("/", (req, res, next) => {
+
+//     console.log("main url")
+//       res.send("<h2>got the response from express!</h2>");
+//     // next();
+// })
+
+
+app.use((req, res, next) => {           // in the last after all routes we are declaring here for 404 req
+    res.status(404)
+       .send('<h2>Page not found</h2>')
 })
 
-app.listen(process.env.PORT || 5000);
+// const server = http.createServer(app)
+// app.listen(process.env.PORT || 5000);
+// server.listen(3003);
+app.listen(3003)
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 npm init -y
 npm i express --save
 node index.js to run the app
+npm install --save body-parser
+after installling a new package 
+npm start
